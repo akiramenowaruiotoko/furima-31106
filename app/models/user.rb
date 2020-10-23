c6bbf0701bafc177c84e1f4745fa7bb409e6a2d5
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2020-10-23 19:18:23 +0900

    userモデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,28 @@
 class User < ApplicationRecord
 
-  validates :nickname        , presence: true
-  validates :last_name       , presence: true
-  validates :first_name      , presence: true
-  validates :last_name_kana  , presence: true
-  validates :first_name_kana , presence: true
-  validates :birth_date      , presence: true
-  
+    validates :nickname        , presence: true
 
-  has_many :items
-  has_many :orders
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  with_options presence:true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :last_name       , presence: true
+    validates :first_name      , presence: true
+  end
+ 
+  with_options presence:true, format: {with: /\A[ァ-ン]+\z/} do
+     validates :last_name_kana  , presence: true
+    validates :first_name_kana , presence: true
+  end
+
+    validates :birth_date      , presence: true
+    
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :encrypted_password, with:PASSWORD_REGEX 
+
+
+    has_many :items
+    has_many :orders
+    
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
 end
