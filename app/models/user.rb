024df9d7b7da27322466cb14127736b290a8f164
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2020-10-25 15:23:34 +0900

    ユーザー登録機能　rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,26 @@
 class User < ApplicationRecord
+  validates :nickname, presence: true
 
-    validates :nickname        , presence: true
-
-  with_options presence:true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/} do
-    validates :last_name       , presence: true
-    validates :first_name      , presence: true
-  end
- 
-  with_options presence:true, format: {with: /\A[ァ-ン]+\z/} do
-    validates :last_name_kana  , presence: true
-    validates :first_name_kana , presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
   end
 
-    validates :birth_date      , presence: true
-    
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX 
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
 
-    has_many :items
-    has_many :orders
-    
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+  validates :birth_date, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  has_many :items
+  has_many :orders
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-        nickname               { Faker::Name.initials(number: 2) }
-        email                  { Faker::Internet.free_email }
-        password               { Faker::Alphanumeric.alphanumeric(number: 6, min_alpha:3, min_numeric: 3) }
-        password_confirmation  { password }
-        last_name              { person.last.kanji }
-        first_name             { person.first.kanji }
-        last_name_kana         { person.last.katakana }
-        first_name_kana        { person.first.katakana }
-        birth_date             { Faker::Date.backward }
+    nickname               { Faker::Name.initials(number: 2) }
+    email                  { Faker::Internet.free_email }
+    password               { Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 3, min_numeric: 3) }
+    password_confirmation  { password }
+    last_name              { person.last.kanji }
+    first_name             { person.first.kanji }
+    last_name_kana         { person.last.katakana }
+    first_name_kana        { person.first.katakana }
+    birth_date             { Faker::Date.backward }
   end
 end
