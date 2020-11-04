f9c26943f82ced461ae0bba17e3c6420bc61b06f
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2020-11-04 17:11:03 +0900

    購入ページの不要なコメント削除とテストコードに電話番号の入力は数字のみを追加

--- spec/models/order_shipping_address_spec.rb
+++ spec/models/order_shipping_address_spec.rb
@@ -57,6 +57,11 @@ RSpec.describe OrderShippingAddress, type: :model do
         @order_shipping_address.valid?
         expect(@order_shipping_address.errors.full_messages).to include('Phone number is invalid')
       end
+      it 'phone_numberが数字のみでないと購入できない' do
+        @order_shipping_address.phone_number = '123456789ab'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include('Phone number is invalid')
+      end
       it 'tokenが存在しないと購入できない' do
         @order_shipping_address.token = nil
         @order_shipping_address.valid?
